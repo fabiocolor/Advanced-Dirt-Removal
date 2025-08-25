8bd1013074fa98851a5f051a399a1debc68132f2
fabiocolor <info@fabiocolor.com>
2025-08-24 21:07:36 -0500

    docs: Ignore Developer directory

--- Developer/Workflow Integrations/Examples/SamplePlugin/js/app.js
+++
@@ -1,8 +0,0 @@
-function Execute() {
-    scriptText = document.getElementById("scriptTextArea").value;
-    window.electronAPI.executeUserScript(scriptText);
-}
-
-function ClearScript() {
-    document.getElementById("scriptTextArea").value = "";
-}
